6a4243fa44994f380a7cf42911dba278931a9f34
Rio Kivell <rjckivel@gmail.com>
2018-01-30 11:51:41 -0500

    Created skeleton code for different solutions, and added a small Makefile to make testing quicker

--- trains.py
+++ trains.py
@@ -1,5 +1,15 @@
 import sys
 
+print(sys.argv)
 
-def function(argc, argv):
+def main(argc, argv):
     print "Initial commit!"
+
+def distance(pointA, pointB):
+    pass
+
+def numRoutes(pointA, pointB, maxdepth):
+    pass
+
+def shortestRoute(pointA, pointB):
+    pass
