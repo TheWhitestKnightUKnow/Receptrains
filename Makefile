fcf474db1622e0197d829eecf746b4639e20502b
Rio Kivell <rjckivel@gmail.com>
2018-01-30 16:42:15 -0500

    Updated the Makefile for some simple test cases, set up main function to pass inputs to appropriate solution function.

--- Makefile
+++ Makefile
@@ -1,3 +1,14 @@
-TESTGRAPH="AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7"
 run:
-	python trains.py $(TESTGRAPH)
+	python trains.py "AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7"
+
+path:
+	python trains.py "AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7" "path" "A-B-C"
+
+routes:
+	python trains.py "AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7" "routes" "C-C-3"
+
+shortest:
+	python trains.py "AB5, BC4, CD8, DC8, DE6, AD5, CE2, EB3, AE7" "shortest" "A-C"
+
+clean:
+	rm -f *.pyc
--- trains.py
+++ trains.py
@@ -5,25 +5,56 @@ from graph import Graph
 # Constructs the graph and decides which solution
 # to employ
 def main(argv):
-    g = Graph()
-    for string in (sys.argv[1].split(", ")):
-        g.add_track(string[0], string[1], int(string[2:]))
+    help_text = """The way to use the trains function is to give it 3 values: trains(graph, option, input)
+graph  : a single string of comma-separated tracks.  eg "AB5, BC7, CA2"
+option : either "path", "routes", or "shortest"
+input  : for path, dash-separated towns, eg "A-B-C"
+         for routes, the two towns to find routes between, and a maximum distance, dash-separated, eg. "A-C-9"
+         for shortest, the two towns to find the shortest route between, dash-separated, eg. "C-B"
+    """
+    if len(argv) > 1:
+        if argv[1] == "-h" or argv[1] == "--help":
+            print help_text
+            exit(0)
+        # Import the graph from the first CLI argument,
+        # a string in the form of comma-separated values
+        # in the form "AB5", to represent a track
+        # connecting the first town ("A") to the second
+        # ("B") with a distance of 5.
+        g = Graph()
+        for string in (sys.argv[1].split(", ")):
+            g.add_track(string[0], string[1], int(string[2:]))
+        if len(argv) > 2:
+            # Determine which function to call based on "option"
+            if argv[2] == "path":
+                path = argv[3].split("-")
+                print distance(path)
+            elif argv[2] == "routes":
+                routes = argv[3].split("-")
+                print numRoutes(routes[0], routes[1], int(routes[2]))
+            elif argv[2] == "shortest":
+                points = argv[3].split("-")
+                print shortestRoute(points[0], points[1])
+            else:
+                print help_text
+        else:
+            print help_text
 
 # Calculate the total distance of a path, or 
 # return 'NO SUCH ROUTE' if it doesn't exist
 def distance(path):
-    pass
+    return path
 
 # Find the number of routes that start at pointA
 # and finish at pointB with a maximum total 
 # distance of maxdistance.  PointA and PointB
 # can be the same point.
 def numRoutes(pointA, pointB, maxdistance):
-    pass
+    return pointA
 
 # Find the shortest route in the graph between
 # pointA and pointB
 def shortestRoute(pointA, pointB):
-    pass
+    return pointA
 
 main(sys.argv)
